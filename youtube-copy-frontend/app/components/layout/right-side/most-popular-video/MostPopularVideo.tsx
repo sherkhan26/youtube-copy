33139341b10c4eb4b895cd937a27cd47732fb0a5
Шерхан Амангелды <sherhan_26@mail.ru>
2025-02-14 09:48:12 +0500

    fix

---
+++ youtube-copy-frontend/app/components/layout/right-side/most-popular-video/MostPopularVideo.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+
+import VideoItem from '@/components/ui/video-item/VideoItem'
+
+import { IVideo } from '@/types/video.interface'
+
+const MostPopularVideo: FC<{ video: IVideo }> = ({ video }) => {
+	return (
+		<div id='live'>
+			<div className='title_gray mb-5'>
+				<h2>Top video</h2>
+			</div>
+
+			<VideoItem item={video} tag={'🔥Hot'} isAvatar />
+		</div>
+	)
+}
+
+export default MostPopularVideo
