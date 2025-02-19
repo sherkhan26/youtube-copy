1cf5ca76834e648ce4facf1f2fa5a189a5be8489
Шерхан Амангелды <sherhan_26@mail.ru>
2025-02-19 12:10:12 +0500

    fix

---
+++ youtube-copy-frontend/app/types/user.interface.ts
@@ -0,0 +1,19 @@
+export interface IUser {
+	_id: string
+	email: string
+	name: string
+	avatarPath: string
+	description: string
+	location: string
+	subscribersCount: number
+	createdAt: string
+	updatedAt: string
+	videosCount?: number
+	isVerified?: boolean
+}
+
+export interface IUserDto
+	extends Pick<
+		IUser,
+		'email' | 'description' | 'name' | 'location' | 'avatarPath'
+	> {}
