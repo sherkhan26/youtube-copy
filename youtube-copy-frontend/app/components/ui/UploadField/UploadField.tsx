037102618af9b70b0bee3c0754b780aa7f2a717f
Шерхан Амангелды <sherhan_26@mail.ru>
2025-02-28 12:32:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/ui/UploadField/UploadField.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react'
+
+import { IUploadField } from '@/components/ui/UploadField/upload-field.interface'
+import { useUploadFile } from '@/components/ui/UploadField/useUploadFile'
+
+import styles from './UploadField.module.scss'
+
+const UploadField: FC<IUploadField> = ({
+	title,
+	onChange,
+	folder,
+	setValue,
+	setIsChosen
+}) => {
+	const { uploadFile } = useUploadFile(onChange, folder, setValue, setIsChosen)
+
+	return (
+		<div className={styles.file}>
+			{title && <h1>{title}</h1>}
+			<label>
+				<span className='sr-only'>Choose File</span>
+				<input type='file' onChange={uploadFile} />
+			</label>
+		</div>
+	)
+}
+
+export default UploadField
