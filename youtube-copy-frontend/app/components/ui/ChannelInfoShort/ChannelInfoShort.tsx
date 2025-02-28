fae82436ac21408f0503401495ee7093932a1112
Шерхан Амангелды <sherhan_26@mail.ru>
2025-02-28 08:31:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/ui/ChannelInfoShort/ChannelInfoShort.tsx
@@ -0,0 +1,32 @@
+import { IUser } from '@/types/user.interface'
+import cn from 'classnames'
+import Image from 'next/image'
+import { FC } from 'react'
+
+import styles from './ChannelInfoShort.module.scss'
+
+const ChannelInfoShort: FC<{ channel: IUser }> = ({ channel }) => {
+	return (
+		<div className={styles.profile_info}>
+			<Image
+				src={channel.avatarPath}
+				alt={channel.name}
+				width={50}
+				height={50}
+				quality={90}
+			/>
+			<div>
+				<div
+					className={cn(styles.name, {
+						verified: channel.isVerified
+					})}
+				>
+					{channel.name}
+				</div>
+				<div className={styles.location}>{channel.location}</div>
+			</div>
+		</div>
+	)
+}
+
+export default ChannelInfoShort
