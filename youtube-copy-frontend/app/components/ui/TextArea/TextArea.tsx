46864c09576a8327073b3fd507bd1a7c53b56ddf
Шерхан Амангелды <sherhan_26@mail.ru>
2025-02-28 11:01:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/ui/TextArea/TextArea.tsx
@@ -0,0 +1,19 @@
+import { FC, forwardRef } from 'react'
+
+import styles from './TextArea.module.scss'
+import { ITextArea } from './text-area.interface'
+
+const TextArea: FC = forwardRef<HTMLTextAreaElement, ITextArea>(
+	({ error, style, ...rest }, ref) => {
+		return (
+			<div className={styles['editor']} style={style}>
+				<textarea ref={ref} {...rest} />
+				{error && <div className={styles.error}>{error.message}</div>}
+			</div>
+		)
+	}
+)
+
+TextArea.displayName = 'TextArea'
+
+export default TextArea
