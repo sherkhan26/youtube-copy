b1b2f9892e3da3518a5e2fe25f4df0f7af8ba622
Шерхан Амангелды <sherhan_26@mail.ru>
2025-03-03 11:15:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/ui/video-item/video-item.interface.ts
@@ -0,0 +1,10 @@
+import { IVideo } from '@/types/video.interface'
+
+export interface IVideoItem {
+	item: IVideo
+	isLarge?: boolean
+	isAvatar?: boolean
+	tag?: string
+	removeHandler?: (videoId: string) => void
+	isUpdateLink?: boolean
+}
