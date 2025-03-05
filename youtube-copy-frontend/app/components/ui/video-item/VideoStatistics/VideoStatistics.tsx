228caff2ecef1c86c93de0a488b1dd62045ea661
Шерхан Амангелды <sherhan_26@mail.ru>
2025-03-05 11:15:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/ui/video-item/VideoStatistics/VideoStatistics.tsx
@@ -0,0 +1,37 @@
+import dayjs from 'dayjs'
+import { FC } from 'react'
+
+import styles from '@/components/ui/video-item/VideoItem.module.scss'
+
+import { formatNumberToK } from '../../../../utils/formatNumberToK'
+
+interface IVideoStatistics {
+	views: number
+	likes?: number
+	createdAt: string
+	subscribers?: number
+}
+
+const VideoStatistics: FC<IVideoStatistics> = ({
+	views,
+	likes,
+	createdAt,
+	subscribers
+}) => {
+	return (
+		<div className={styles.number_info}>
+			<div className={styles.views}>VIEWS {formatNumberToK(views)}</div>
+			{!!likes && (
+				<div className={styles.likes}>LIKES {formatNumberToK(likes)}</div>
+			)}
+			<div className={styles.date}>{dayjs(new Date(createdAt)).fromNow()}</div>
+			{!!subscribers && (
+				<div className={styles.subscribers}>
+					SUBSCRIBERS {formatNumberToK(subscribers)}
+				</div>
+			)}
+		</div>
+	)
+}
+
+export default VideoStatistics
