462ca0ad0ece369542878fe0edeff080ee098f6d
Шерхан Амангелды <sherhan_26@mail.ru>
2025-03-10 11:15:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/pages/home/Home.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react'
+
+import { IHome } from '@/components/pages/home/home.interface'
+
+import { useAuth } from '@/hooks/useAuth'
+
+import Layout from '../../layout/Layout'
+import RightSide from '../../layout/right-side/RightSide'
+import Line from '../../ui/Line'
+
+import Recommended from './recommended/Recommended'
+import WeeklyFeatured from './weekly-featured/WeeklyFeatured'
+
+const Home: FC<IHome> = (props) => {
+	const { user } = useAuth()
+
+	return (
+		<Layout title='Youtube v2.0 - Best video'>
+			<div id='wrapper_content'>
+				<div className='left_side'>
+					<WeeklyFeatured
+						weeklyVideos={props.weeklyVideos}
+						randomVideo={props.randomVideo}
+					/>
+					<Line />
+					<Recommended newVideos={props.newVideos} />
+				</div>
+
+				<RightSide topVideo={props.topVideo} topChannels={props.topChannels} />
+			</div>
+		</Layout>
+	)
+}
+
+export default Home
