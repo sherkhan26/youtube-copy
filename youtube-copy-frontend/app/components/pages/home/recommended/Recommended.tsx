f2ed518378d7796edd2d9972cf038e16232e4783
Шерхан Амангелды <sherhan_26@mail.ru>
2025-03-12 11:15:15 +0500

    fix

---
+++ youtube-copy-frontend/app/components/pages/home/recommended/Recommended.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react'
+
+import VideoItem from '@/components/ui/video-item/VideoItem'
+
+import { IVideo } from '@/types/video.interface'
+
+const Recommended: FC<{
+	newVideos: IVideo[]
+	removeHandler?: (videoId: string) => void
+	isUpdateLink?: boolean
+}> = ({ newVideos, removeHandler, isUpdateLink }) => {
+	return (
+		<div id='recommended'>
+			<div className='top_block'>
+				<div className='left_title title_gray'>
+					<h2>Newest video</h2>
+				</div>
+				<div className='sort'>By View Trending</div>
+			</div>
+
+			<div className='catalog'>
+				{newVideos.map((video) => (
+					<VideoItem
+						item={video}
+						key={video._id}
+						isAvatar
+						removeHandler={removeHandler}
+						isUpdateLink={isUpdateLink}
+					/>
+				))}
+			</div>
+		</div>
+	)
+}
+
+export default Recommended
